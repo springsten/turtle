440cce3b26d80d4c8bb639e8fe6a2b1282d8d28f
mumis <emil.wertwein@gmail.com>
2018-10-11 13:01:14 +0200

    add admin sign in validation

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-function validateSignIn {
-    var form = document.getElementById("");
-    
+var signInSubmit = document.getElementById("signInSubmit");
+
+signinReq.addEventListener("click", validateSignIn);
+
+function validateSignIn() {
+    var form = document.getElementById("admin-login");
+    var email = form.email.value;
+    var password = form.password.value;
+    if (!email == "charlie.brown@gmail.com" || !password == "admin"){
+        return
+    }
+    else {
+        window.location.href = "adminpage.html";
+    }
 }
